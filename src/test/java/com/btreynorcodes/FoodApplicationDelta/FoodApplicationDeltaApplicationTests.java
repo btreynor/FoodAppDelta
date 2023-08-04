ad04025824844c754ceb7e64f70af8c37f814fb1
btreynor8719 <btreynor213@bears.shawu.edu>
2023-08-04 13:22:51 -0400

    initial commit

---
+++ src/test/java/com/btreynorcodes/FoodApplicationDelta/FoodApplicationDeltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btreynorcodes.FoodApplicationDelta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodApplicationDeltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
